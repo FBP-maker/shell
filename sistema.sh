4859d86e6c50ecc021642b926ffda7daac3ea5d5
FBP-maker <filipe_bastos5@hotmail.com>
2021-10-21 20:51:17 -0300

    adicionando comentários em sistema.sh e adicionando arquivos

--- sistema.sh
+++ sistema.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-# sistema - sript que mostra informções sobre o sistema
-# Autor: Fulano da Silva
+# sistema - script que mostra informações sobre o sistema
+# Autor: Fulando da Silva
 
-echo "Data e Horário:"
+# Peder uma confirmação do usuário antes de executar o script
+echo "Vou buscar os dados do sistema. Posso continuar? Digite [s] para sim ou, [n] para não."
+read RESPOSTA
+
+# Se digitado 'n', vamos interromper o script
+test "$RESPOSTA" = "n" && exit
+
+# O date mostra a data e a hora correntes
+echo "Data e Horário"
 date
 
-echo "Uso do disco:"
+# O df mostra as partições e quanto cada uma ocupa no disco
+echo "Uso do disco"
 df
 
-echo "Usuários conectados:"
+# O w mostra os usuários que estão conectados nesta máquina
+echo "Usuários conectados"
 w

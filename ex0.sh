5924c609ed30e70a3eb535b6e023e230e243ef9c
FBP-maker <filipe_bastos5@hotmail.com>
2021-10-24 15:49:47 -0300

    aula 0; usando echo, '',  e \.

---
+++ ex0.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#usando aspas simples
+
+echo 'sem expanção da variável $SHELL'
+
+#usando aspas duplas
+
+echo "com expanção da variável $SHELL"
+
+#usando caracteres de escape
+
+echo "olá tudo bem \"outro texto\""
+
+echo "\$SHELL"
+
+#sem caracter de escape
+
+echo "olá tudo bem "outro texto""
+
+echo "$SHELL"
+

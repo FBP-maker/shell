1020fd6e8b910280f0b08495e7025e4d4cd47893
FBP-maker <filipe_bastos5@hotmail.com>
2021-10-26 18:54:06 -0300

    corrigindo cronometro

--- cronometro.sh
+++ cronometro.sh
@@ -1,9 +1,9 @@
-#!/usr/bim/env bash
+#!/usr/bin/env bash
 
 contadora="0"
 
 while sleep 1s; do
-	[[ "$contadora" -eq "120"]] && echo "Acabou o tempo" || let contadora++
+	[[ "$contadora" -eq "5" ]] && echo "Acabou o tempo" || let contadora++ 
 done
 
 #Corrigir problemas aula 1

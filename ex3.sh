04dbed99261d23247dc087b23c8596dc22ad1dc4
FBP-maker <filipe_bastos5@hotmail.com>
2021-10-26 19:20:50 -0300

    variáveis de ambiente

---
+++ ex3.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#varáveis de ambiente.
+
+echo "Qual a linguagem do Sistema"
+echo "$LANG"
+
+echo "Qual o diretório do Usuário Logado?"
+echo "$HOME"
+
+echo "Qual o nome do usuário?"
+echo "$USER"

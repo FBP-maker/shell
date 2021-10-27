5ea23068e1dc18288b42178a9f62eb5eaa57f832
FBP-maker <filipe_bastos5@hotmail.com>
2021-10-27 09:26:20 -0300

    Srcipt para criar pendrive botável

---
+++ boot.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo -e "Identifique dispositivo que receberá a imagem: \n  $(fdisk -l)"
+
+read -p "Indique o caminho do dispositivo:" PENDRIVE
+
+read -p "Forneça o caminho da ISO:" ISO
+
+dd if=$ISO of=$PENDRIVE status=progress && sync 

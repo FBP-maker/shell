58736c9f10d188e5ecd92458a5d7c625cef5bd96
FBP-maker <filipe_bastos5@hotmail.com>
2021-10-27 19:31:25 -0300

    adicionando comando mkfs.fat -I para formatação do dispositivo

--- boot.sh
+++ boot.sh
@@ -4,6 +4,8 @@ echo -e "Identifique dispositivo que receberá a imagem: \n  $(fdisk -l)"
 
 read -p "Indique o caminho do dispositivo:" PENDRIVE
 
+mkfs.vfat -I $PENDRIVE
+
 read -p "Forneça o caminho da ISO:" ISO
 
 dd if=$ISO of=$PENDRIVE status=progress && sync 

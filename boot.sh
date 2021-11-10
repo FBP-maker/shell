26790a897f6015ffad98d2bd5b2bb9b5f32900c1
FBP-maker <filipe_bastos5@hotmail.com>
2021-11-10 07:49:43 -0300

    adicionando testes em boot.sh

--- boot.sh
+++ boot.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+#testa internet
+if ! wget -q --spider www.google.com; then
+	echo "Necessita de internet para realizar está operação"
+	exit 1
+fi
+
+#Testa se o usuário é root
+[[ "$UID" -ne "0" ]] && { echo "Necessita de root" ; exit1 ;}
+
 echo -e "Identifique dispositivo que receberá a imagem: \n  $(fdisk -l)"
 
 read -p "Indique o caminho do dispositivo:" PENDRIVE

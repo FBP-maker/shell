07453dce4a68988f70cfe069c3941297ff449b53
FBP-maker <filipe_bastos5@hotmail.com>
2021-12-04 09:17:56 -0300

    editando baixa videos

--- baixa_video.sh
+++ baixa_video.sh
@@ -5,12 +5,13 @@
 
 #==========VÁRIAVEIS DO PROGRAMA=============#
 # 64K, 128K, 256K, 320K.
-qualidade="320"
+qualidade="320k"
 
 # BEST, aac, vorbis, m4a, opus, wav
 formato_audio="mp3"
 
 #==========TESTES INICIAIS====================#
-[[ "$UID" -eq "0" ]] && { echo "Root? não" ; exit 1;}
+[[ "$UID" -eq "0" ]] && { echo "Root? não" ; exit 1 ;}
 
-youtube-dl --embed-thumbnail --audio-quality "$qualidade" --extract-audio --audio-format "$formato_audio" "$0"
+youtube-dl --embed-thumbnail --audio-quality "$qualidade"  "$@"
+#--extract-audio "$formato_audio"

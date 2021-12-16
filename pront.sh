1dfca537ea81e391ca89badf30539da56137cc36
FBP-maker <filipe_bastos5@hotmail.com>
2021-12-16 19:45:14 -0300

    futricando

--- pront.sh
+++ pront.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 #Modificando o visual do pront
-echo "PS1=' [$LOGNAME]-$(date +"%d/%m/%Y") as $(date +"%H:%M")-$HOME=>'" >> .bashrc
+echo "PS1=' [$LOGNAME]-$(date +"%d/%m/%Y") as $(date +"%H:%M")-$(pwd)=>'" >> .bashrc

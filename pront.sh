3f53155f04163ee1f50b2cb2258707f2c3eafe91
FBP-maker <filipe_bastos5@hotmail.com>
2022-02-21 12:06:05 -0300

    correção e adição de função em pront.sh

--- pront.sh
+++ pront.sh
@@ -1,3 +1,10 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 #Modificando o visual do pront
-echo 'PS1=' [$LOGNAME]-$(date +"%d/%m/%Y") as $(date +"%H:%M")-$(pwd)=>'' >> .bashrc
+echo "PS1=' [\$LOGNAME]-\$(date +"%d/%m/%Y") as \$(date +"%H:%M")-\$(pwd)=>\\$ '" >> /home/fbp/.bashrc
+
+echo \
+"
+set number 
+set mouse=r 
+syntax on 
+" >> /home/fbp/.vimrc

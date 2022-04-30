33a939e901e89bb4204ae817d6046c6d04885fb7
FBP-maker <filipe_bastos5@hotmail.com>
2022-04-30 13:02:13 -0300

    correções

--- pront.sh
+++ pront.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 #Modificando o visual do pront
-echo \
-"
-PS1=' [\$LOGNAME]-\$(date +"%d/%m/%Y") as \$(date +"%H:%M")-\$(pwd)=>\\$ '
-PATH=PATH:
-" >> ~/.bashrc
+echo '\
+PS1=" [\u]-$(date +"%d/%m/%Y") às $(date +"%H:%M")-[\w]=>\$ "
+' >> ~/.bashrc
 
-echo \
-"
+echo "\
 set number 
 set mouse=r 
 syntax on 
